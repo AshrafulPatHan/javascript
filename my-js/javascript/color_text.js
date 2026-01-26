f887fa470be5f6e0536ec91ccfe180771b737598
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-01-26 18:58:22 +0600

    add many color options

--- my-js/javascript/color_text.js
+++ my-js/javascript/color_text.js
@@ -1,3 +1,5 @@
+// option 1
+
 const red = '\x1b[31m';
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
@@ -5,3 +7,28 @@ const reset = '\x1b[0m';
 console.log(red + 'This text is red.' + reset);
 console.log(green + 'This text is green.' + reset);
 console.log('\x1b[33m%s\x1b[0m', 'This text is yellow.'); // Alternative with placeholder
+
+// option 2
+
+const colorize = (...args) => ({
+  black: `\x1b[30m${args.join(' ')}`,
+  red: `\x1b[31m${args.join(' ')}`,
+  green: `\x1b[32m${args.join(' ')}`,
+  yellow: `\x1b[33m${args.join(' ')}`,
+  blue: `\x1b[34m${args.join(' ')}`,
+  magenta: `\x1b[35m${args.join(' ')}`,
+  cyan: `\x1b[36m${args.join(' ')}`,
+  white: `\x1b[37m${args.join(' ')}`,
+  bgBlack: `\x1b[40m${args.join(' ')}\x1b[0m`,
+  bgRed: `\x1b[41m${args.join(' ')}\x1b[0m`,
+  bgGreen: `\x1b[42m${args.join(' ')}\x1b[0m`,
+  bgYellow: `\x1b[43m${args.join(' ')}\x1b[0m`,
+  bgBlue: `\x1b[44m${args.join(' ')}\x1b[0m`,
+  bgMagenta: `\x1b[45m${args.join(' ')}\x1b[0m`,
+  bgCyan: `\x1b[46m${args.join(' ')}\x1b[0m`,
+  bgWhite: `\x1b[47m${args.join(' ')}\x1b[0m`
+});
+console.log(colorize('foo').red); // 'foo' (red letters)
+console.log(colorize('foo', 'bar').bgBlue); // 'foo bar' (blue background)
+console.log(colorize(colorize('foo').yellow, colorize('foo').green).bgWhite);
+// 'foo bar' (first word in yellow letters, second word in green letters, white background for both)
