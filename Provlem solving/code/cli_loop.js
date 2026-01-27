a4425ac789e10d8e08db771c87d5372d7815768a
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-01-27 12:32:56 +0600

    plaing for a new provlem solve

---
+++ Provlem solving/code/cli_loop.js
@@ -0,0 +1,10 @@
+/*
+
+  start app
+  app ask for input a letter
+  user input a letter on terminal
+  app return how many text on this letter
+  app again ask for input a letter
+  app is not end if user type "exit" app is then close
+
+*/
